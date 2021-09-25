d4ab7be392bca16ed275c93bd9a609978e0074c6
Anton Nekipelov <226657+anton-107@users.noreply.github.com>
2021-09-25 13:35:54 +0200

    cache queries to github

---
+++ src/github-client/github-client-inmemory-cache.ts
@@ -0,0 +1,63 @@
+import { GitHubClient, GitHubCommit, GitHubFork, GitHubRepository, GitHubSearchResult, GitHubUser } from "./interfaces";
+
+export class GitHubClientWithInMemoryCache implements GitHubClient {
+  private searchCache: Record<string, Record<number, GitHubSearchResult>> = {};
+  private repositoryCache: Record<string, Record<string, GitHubRepository>> = {};
+  private commitsCache: Record<string, GitHubCommit[]> = {};
+  private forksCache: Record<string, GitHubFork[]> = {};
+  private userCache: Record<string, GitHubUser> = {};
+
+  constructor(private nonCachedClient: GitHubClient) {}
+
+  public async searchRepositories(query: string, page: number): Promise<GitHubSearchResult> {
+    if (this.searchCache[query] && this.searchCache[query][page]) {
+      console.log('searchRepositories cache hit');
+      return this.searchCache[query][page];
+    }
+    const results = await this.nonCachedClient.searchRepositories(query, page);
+    if (!this.searchCache[query]) {
+      this.searchCache[query] = {};
+    }
+    this.searchCache[query][page] = results;
+    return results;
+  }
+  public async getRepository(owner: string, name: string): Promise<GitHubRepository> {
+    if (this.repositoryCache[owner] && this.repositoryCache[owner][name]) {
+      console.log('getRepository cache hit');
+      return this.repositoryCache[owner][name];
+    }
+    const results = await this.nonCachedClient.getRepository(owner, name);
+    if (!this.repositoryCache[owner]) {
+      this.repositoryCache[owner] = {};
+    }
+    this.repositoryCache[owner][name] = results;
+    return results;
+  }
+  public async getCommits(commitsURL: string): Promise<GitHubCommit[]> {
+    if (this.commitsCache[commitsURL]) {
+      console.log('getCommits cache hit');
+      return this.commitsCache[commitsURL];
+    }
+    const results = await this.nonCachedClient.getCommits(commitsURL);
+    this.commitsCache[commitsURL] = results;
+    return results;
+  }
+  public async getForks(forksURL: string): Promise<GitHubFork[]> {
+    if (this.forksCache[forksURL]) {
+      console.log('getForks cache hit');
+      return this.forksCache[forksURL];
+    }
+    const results = await this.nonCachedClient.getForks(forksURL);
+    this.forksCache[forksURL] = results;
+    return results;
+  }
+  public async getUser(userURL: string): Promise<GitHubUser> {
+    if (this.userCache[userURL]) {
+      console.log('getUser cache hit');
+      return this.userCache[userURL];
+    }
+    const results = await this.nonCachedClient.getUser(userURL);
+    this.userCache[userURL] = results;
+    return results;
+  }
+}
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { GitHubSearchApp } from "./components/main.app";
 import { GitHubClientImpl } from "./github-client/github-client";
+import { GitHubClientWithInMemoryCache } from "./github-client/github-client-inmemory-cache";
 import { FetchWrapper } from "./web-api/fetch-wrapper";
 
 const fetchWrapper = new FetchWrapper();
 const gitHubClient = new GitHubClientImpl({numberOfRepositoriesPerPage: 10}, fetchWrapper);
-const App = () => <GitHubSearchApp githubClient={gitHubClient} />;
+const gitHubClientInMemoryCache = new GitHubClientWithInMemoryCache(gitHubClient);
+const App = () => <GitHubSearchApp githubClient={gitHubClientInMemoryCache} />;
 
 ReactDOM.render(
   <React.StrictMode>
